1516dc6c83c563877d67564bfd0603128a004565
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-10 14:44:19 +0100

    Bonus: move article in its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Form from "./components/Form";
+import Article from "./components/Article";
 
 function App() {
 	const articleList = [
@@ -38,15 +39,6 @@ function App() {
 	const [articles, setArticles] = useState(articleList);
 	const [newArticle, setNewArticle] = useState(emptyArticle);
 
-	const handleRemoveArticle = (index) => {
-		const updatedList = articles.filter((_, articleIndex) => {
-			return articleIndex !== index;
-		});
-
-		setArticles(updatedList);
-		console.log(`Removed successfully!`);
-	};
-
 	return (
 		<div className="container">
 			<h1>React Blog</h1>
@@ -59,24 +51,13 @@ function App() {
 
 			<ul className="article-list">
 				{articles.map((item, index) => (
-					<li className="article-title" key={`${index}-${item.title}`}>
-						{item.title}
-						<button type="submit" onClick={() => handleRemoveArticle(index)}>
-							{/* I don't want to install bootstrap icons for a single icon */}
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="16"
-								height="16"
-								fill="currentColor"
-								className="bi bi-trash"
-								viewBox="0 0 16 16"
-							>
-								<title>Remove</title>
-								<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-								<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-							</svg>
-						</button>
-					</li>
+					<Article
+						key={`${index}-${item.title}`}
+						articles={articles}
+						setArticles={setArticles}
+						item={item}
+						index={index}
+					/>
 				))}
 			</ul>
 		</div>

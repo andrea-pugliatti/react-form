fe1caeb5d006c33a7115ea7a30674ee98007330c
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-10 13:11:51 +0100

    Handle add article logic

--- src/App.jsx
+++ src/App.jsx
@@ -27,21 +27,46 @@ function App() {
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias eveniet amet aut. Odio laboriosam cupiditate natus temporibus qui modi minus, itaque sit! A, doloribus. Modi velit consectetur sunt optio.",
 		},
 	];
+
+	const emptyArticle = {
+		id: 0,
+		title: "",
+		article: "",
+	};
+
 	const [articles, setArticles] = useState(articleList);
-	const [newArticle, setNewArticle] = useState("");
+	const [newArticle, setNewArticle] = useState(emptyArticle);
+
+	const handleAddArticle = (event) => {
+		event.preventDefault();
+		const updatedList = [...articles, newArticle];
+		setArticles(updatedList);
+		console.log(`Added task: ${newArticle}`);
+		console.log(event);
+	};
 
 	return (
 		<div className="container">
 			<h1>React Blog</h1>
 
-			<form className="add-article">
-				<input type="text" />
+			<form onSubmit={handleAddArticle} className="add-article">
+				<input
+					type="text"
+					value={newArticle.title}
+					onChange={(event) =>
+						setNewArticle({
+							id: articles.length + 1,
+							title: event.target.value,
+							article: "Lorem ipsum",
+						})
+					}
+				/>
 				<button type="submit">Aggiungi</button>
 			</form>
 
 			<ul className="article-list">
-				{articleList.map((item) => (
-					<li className="article-title" key={item.id}>
+				{articles.map((item, index) => (
+					<li className="article-title" key={`${index}-${item.title}`}>
 						{item.title}
 					</li>
 				))}

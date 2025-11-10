9db16365967ef90902557ca14005f55ad55854a6
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-10 12:44:20 +0100

    Add list of article titles

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,42 @@ function App() {
 	const articleList = [
 		{
 			id: 1,
-			title: "Lorem ipsum dolor",
+			title: "Il Magico Mondo",
 			article:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias eveniet amet aut. Odio laboriosam cupiditate natus temporibus qui modi minus, itaque sit! A, doloribus. Modi velit consectetur sunt optio.",
 		},
 		{
 			id: 2,
-			title: "Lorem ipsum",
+			title: "Dei cassetti e altre cose",
 			article:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias eveniet amet aut. Odio laboriosam cupiditate natus temporibus qui modi minus, itaque sit! A, doloribus. Modi velit consectetur sunt optio.",
 		},
 		{
 			id: 3,
-			title: "Lorem ipsum dolor sit",
+			title: "Come evitare oggetti in volo",
 			article:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias eveniet amet aut. Odio laboriosam cupiditate natus temporibus qui modi minus, itaque sit! A, doloribus. Modi velit consectetur sunt optio.",
 		},
 		{
 			id: 4,
-			title: "Lorem ipsum dolor sit amet",
+			title: "Come fare ad arrivare a fine corso",
 			article:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias eveniet amet aut. Odio laboriosam cupiditate natus temporibus qui modi minus, itaque sit! A, doloribus. Modi velit consectetur sunt optio.",
 		},
 	];
 
-	return <></>;
+	return (
+		<div className="container">
+			<h1>React Blog</h1>
+			<ul className="article-list">
+				{articleList.map((item) => (
+					<li className="article-title" key={item.id}>
+						{item.title}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
